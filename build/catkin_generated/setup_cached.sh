2bde21d097c2898732dceb9e2288725bd047c889
Akshit Kumar <akshitkumar100@gmail.com>
2016-12-09 07:12:24 +0530

    make the workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/akshitk/rover-control/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PKG_CONFIG_PATH="/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/akshitk/rover-control/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/akshitk/rover-control/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/akshitk/rover-control/src:$ROS_PACKAGE_PATH"
