2d2fae712aa99dea3a042e4421edf70e63eda85d
AkshitKumar <akshitkumar100@gmail.com>
2016-12-18 23:26:55 +0530

    Remove build and devel folders

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akshitk/rover-control/src
-# Build directory: /home/akshitk/rover-control/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/learning_joy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/akshitk/rover-control/src/learning_joy
-# Build directory: /home/akshitk/rover-control/build/learning_joy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
