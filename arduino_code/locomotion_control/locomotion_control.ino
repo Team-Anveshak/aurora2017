02f730719a1cc1625523f3ab79555617de465651
koriavinash1 <koriavinash1@gmail.com>
2016-12-25 14:45:14 +0530

    update locomtion_control arduino

--- arduino_code/locomotion_control/locomotion_control.ino
+++ arduino_code/locomotion_control/locomotion_control.ino
@@ -5,26 +5,23 @@
 #include <ros.h>
 #include <Servo.h> 
 #include <rover_msgs/WheelVelocity.h>
-#include <rover_msgs/CameraMotion.h>
-#include <std_msgs/String.h>
-#define dcmotor_tl 3  //tl-DR1
-#define dcmotor_tr 5  //tr-DR1
-#define dcmotor_bl 9 //bl-DR2
-#define dcmotor_br 10 //br-DR3
-#define dcmotor_br2 6
-#define dcmotor_bl2 11
+#define dcmotor_fl 3 
+#define dcmotor_fr 5  
+#define dcmotor_ml 6 
+#define dcmotor_mr 9
+#define dcmotor_br 10
+#define dcmotor_bl 11
 
-ros::NodeHandle nh;
+ros::NodeHandle_<ArduinoHardware, 5, 5, 125, 125> nh;
 
-Servo driveServo1;
-Servo steerServo1;
-Servo driveServo2;
-Servo driveServo3;
-Servo steerServo2;
-Servo steerServo3;
+Servo frontLeft;
+Servo frontRight;
 
-//rover_msgs::CameraMotion camFeedback[4];
-//rover_msgs::WheelVelocity wheelFeedback[6];
+Servo middleLeft;
+Servo middleRight;
+
+Servo backLeft;
+Servo backRight;
 
 /*
 *RoverVelocity is an array of the velocity values in order:
@@ -36,40 +33,35 @@ Servo steerServo3;
 * 5 right_back_vel
 */
 
-int tl = 0,tr = 0,bl = 0,br = 0;
+int fl = 0,fr = 0, ml = 0, mr = 0, bl = 0, br = 0;
 
-void Locomotion(int tl,int tr,int bl,int br)
+void Locomotion()
 {
-//SetSpeed(servoAddr1,theta); // theta between -2147483648 and 2147483648 
-//SetSpeed(servoAddr2,theta);
-driveServo1.writeMicroseconds(tl);
-steerServo1.writeMicroseconds(tr);
-driveServo2.writeMicroseconds(bl);
-//steerServo2.writeMicroseconds(1500);
-driveServo3.writeMicroseconds(br);
-//steerServo2.writeMicroseconds(1500);
+  frontLeft.writeMicroseconds(fl);
+  frontRight.writeMicroseconds(fr);
   
+  middleLeft.writeMicroseconds(ml);
+  middleRight.writeMicroseconds(mr);
+  
+  backLeft.writeMicroseconds(bl);
+  backRight.writeMicroseconds(br);
 }
 
 void roverMotionCallback(const rover_msgs::WheelVelocity& RoverVelocity){
-  str_msg.data = "hello";
-  chatter.publish( &str_msg );
-  tl = (int)RoverVelocity.left_front_vel;
-  tr = (int)RoverVelocity.right_front_vel;
-  bl = (int)RoverVelocity.left_vel;
-  br = (int)RoverVelocity.right_vel;
+  fl = (int)RoverVelocity.left_front_vel;
+  fr = (int)RoverVelocity.right_front_vel;
+  
+  ml = (int)RoverVelocity.left_middle_vel;
+  mr = (int)RoverVelocity.right_middle_vel;
+  
+  bl = (int)RoverVelocity.left_back_vel;
+  br = (int)RoverVelocity.right_back_vel;
 
-  tl=(tl+100)*10;
-  tr=(tr+100)*10;
-  bl=(bl+100)*10;
-  br=(br+100)*10;
+  if(abs(fl)<25){
+     fl=25;fr=25;ml=25;mr=25;bl=25;br=25;
+  }
   
-  tl=tl/2+750;
-  tr=tr/2+750;
-  bl=bl/2+750;
-  br=br/2+750;
-  
-  Locomotion(tl,tr,bl,br);
+  Locomotion();
 }
 
 ros::Subscriber<rover_msgs::WheelVelocity> locomotion_sub("rover1/wheel_vel", &roverMotionCallback);
@@ -78,18 +70,19 @@ void setup(){
   nh.initNode();
   nh.subscribe(locomotion_sub);
 
-driveServo1.attach(dcmotor_tl,1000,2000);            
-steerServo1.attach(dcmotor_tr,1000,2000);             
-driveServo2.attach(dcmotor_bl,1000,2000);
-steerServo2.attach(dcmotor_bl2,1000,2000);
-driveServo3.attach(dcmotor_br,1000,2000);
-steerServo3.attach(dcmotor_br2,1000,2000);
-driveServo1.writeMicroseconds(1500);
-steerServo1.writeMicroseconds(1500);
-driveServo2.writeMicroseconds(1500);
-steerServo2.writeMicroseconds(1500);
-driveServo3.writeMicroseconds(1500);
-steerServo3.writeMicroseconds(1500);
+  frontLeft.attach(dcmotor_fl,1000,2000);            
+  frontRight.attach(dcmotor_fr,1000,2000);             
+  middleLeft.attach(dcmotor_ml,1000,2000);
+  middleRight.attach(dcmotor_mr,1000,2000);
+  backLeft.attach(dcmotor_bl,1000,2000);
+  backRight.attach(dcmotor_br,1000,2000);
+
+  frontLeft.writeMicroseconds(1500);
+  frontRight.writeMicroseconds(1500);
+  middleLeft.writeMicroseconds(1500);
+  middleRight.writeMicroseconds(1500);
+  backLeft.writeMicroseconds(1500);
+  backRight.writeMicroseconds(1500);
 
 }
 
