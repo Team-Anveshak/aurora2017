da4d231aace6ab5cc9a9af0e14fc12bd284f5c89
koriavinash <koriavinash1@gmail.com>
2017-01-10 00:47:38 +0530

    initial_commit

--- install/lib/python2.7/dist-packages/rover_msgs/msg/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._ArmAngle import *
-from ._CameraMotion import *
-from ._GripperMotion import *
-from ._WheelVelocity import *
--- install/share/rover_msgs/cmake/rover_msgs-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(rover_msgs_MESSAGE_FILES "msg/WheelVelocity.msg;msg/CameraMotion.msg;msg/ArmAngle.msg;msg/GripperMotion.msg")
-set(rover_msgs_SERVICE_FILES "")
