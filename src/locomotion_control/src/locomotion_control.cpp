a3c2c2e185f476f7ed96d805817198db021b242f
aniketmore234 <aniket.more305@gmail.com>
2017-01-21 22:09:31 +0530

    diff code

--- src/locomotion_control/src/locomotion_control.cpp
+++ src/locomotion_control/src/locomotion_control.cpp
@@ -23,25 +23,33 @@ LocomotionControl::LocomotionControl(){
 
 void LocomotionControl::joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
     rover_msgs::WheelVelocity vel;
-    double x_axis_val = joy->axes[0];
-    double y_axis_val = joy->axes[1];
+    float x_axis_val = joy->axes[0];
+    float y_axis_val = joy->axes[1];
 
-    double scale = sqrt(x_axis_val * x_axis_val + y_axis_val * y_axis_val);
-    
-/*    if(scale<=0.2){
-        x_axis_val=0;
+    float scale = sqrt(x_axis_val * x_axis_val + y_axis_val * y_axis_val);
+    //ROS_INFO_STREAM("scale : " << scale);
+    float e= 1;
+
+    if(fabs(y_axis_val)<=0.2){
         y_axis_val=0;
-    }*/
+    }
+
+    else if (y_axis_val>0.2){
+     y_axis_val= y_axis_val*5.0/4.0*(e-0.5) + (2.5-e)/4.0;
+    }
+
+    else if (y_axis_val<-0.2){
+     y_axis_val= y_axis_val*5.0/4.0*(e-0.5) - (2.5-e)/4.0;
+    }
     //double angle = atan2(y_axis_val,x_axis_val) * 180 / PI;
-    // double top_left, top_right, bottom_left, bottom_right;
-    //ROS_INFO_STREAM("Scale : " << scale << "Angle : "<< angle);
+    
+    ROS_INFO_STREAM("y : " << y_axis_val << "x : "<< x_axis_val);
 
 float b = 45;
 float a = 110;
 float c2 = 500;
 float c1 = 40;
 
-float n = 1.5; //scaling factor
 
 //% velocity algorithm
 
@@ -52,106 +60,74 @@ float dem = (1+ b/r)*(1+ b/r) + (a/2/r)*(a/2/r);
 
 float v1 = v;
 float v2 = v*( (1+b/r)*(1+b/r)/dem);
-float v3 = v* ((1+b/r)*(1+a/2/r)*(1+a/2/r)/dem);
+float v3 = v* ((1+b/r)*(1+(a/2/r*a/2/r))/dem);
 float v4 = v*((1+b/r)/dem);
 
-    vel.left_front_vel = 1500;
-    vel.right_front_vel = 1500;
-    vel.left_middle_vel = 1500;
-    vel.right_middle_vel = 1500;
-    vel.left_back_vel = 1500;
-    vel.right_back_vel = 1500;
-/*%velocity assignment*/
-/*vlt=v1;
-vlb=v1;
-vlm=v2;
-vrm=v4;
-vrt=v3;x
-vrb=v3;*/
-if(y_axis_val>=0.1){
+ROS_INFO_STREAM("a ::" << 1+b/r << "b: "<< (1+a/2/r)*(1+a/2/r)<< "dem: "<< dem);
+
+
 if(x_axis_val >= 0){
-    if(x_axis_val>0.8 && (fabs(y_axis_val) <=0.2)){
-    vel.left_front_vel = c2/n + 1500;
-    vel.right_front_vel = -c2/n + 1500;
-    vel.left_middle_vel = c2/n + 1500;
-    vel.right_middle_vel = -c2/n + 1500;
-    vel.left_back_vel = c2/n + 1500;
-    vel.right_back_vel = -c2/n + 1500;
-    }
-    else{
-    vel.left_front_vel = v1/5 + 1700;
-    vel.right_front_vel = v3/5 + 1700;
-    vel.left_middle_vel = v4/5 + 1700;
-    vel.right_middle_vel = v2/5 + 1700;
-    vel.left_back_vel = v1/5 + 1700;
-    vel.right_back_vel = v3/5 + 1700;
-    }
-    // thetaL=asin((a/2/r)/sqrt(dem));
-    // thetaR=asin((a/2/r)/sqrt(1+(a/2/r)^2));    
+        if(fabs(y_axis_val) <= 0.2){
+            if (fabs(x_axis_val)<=0.5){
+                vel.left_front_vel = 1500;
+                vel.right_front_vel = 1500;
+                vel.left_middle_vel = 1500;
+                vel.right_middle_vel = 1500;
+                vel.left_back_vel = 1500;
+                vel.right_back_vel = 1500;
+            }
+            else if(x_axis_val>=0.5){
+                float x_axis_val_mod1= x_axis_val*0.4+0.3;
+                vel.left_front_vel = -c2*x_axis_val_mod1+1500;
+                vel.right_front_vel = c2*x_axis_val_mod1+1500;
+                vel.left_middle_vel = -c2*x_axis_val_mod1+1500;
+                vel.right_middle_vel = c2*x_axis_val_mod1+1500;
+                vel.left_back_vel = -c2*x_axis_val_mod1+1500;
+                vel.right_back_vel = c2*x_axis_val_mod1+1500;
+            }
+        }
+        else{
+            vel.left_front_vel = v3 + 1500;
+            vel.right_front_vel = v1 + 1500;
+            vel.left_middle_vel = v2 + 1500;
+            vel.right_middle_vel = v4 + 1500;
+            vel.left_back_vel = v3 + 1500;
+            vel.right_back_vel = v1 + 1500;
+        }
+
 }
+
 else if(x_axis_val < 0){
-    if(x_axis_val<-0.8 && (fabs(y_axis_val) <= 0.2)){
-    vel.left_front_vel = -c2/n + 1500;
-    vel.right_front_vel = c2/n + 1500;
-    vel.left_middle_vel = -c2/n + 1500;
-    vel.right_middle_vel = c2/n + 1500;
-    vel.left_back_vel = -c2/n + 1500;
-    vel.right_back_vel = c2/n + 1500;
-    }
-    else{
-    vel.left_front_vel = v3/5 + 1700;
-    vel.right_front_vel = v1/5 + 1700;
-    vel.left_middle_vel = v2/5 + 1700;
-    vel.right_middle_vel = v4/5 + 1700;
-    vel.left_back_vel = v3/5 + 1700;
-    vel.right_back_vel = v1/5 + 1700;
-    }// thetaL=asin((a/2/r)/sqrt(dem));
-    // thetaR=asin((a/2/r)/sqrt(1+(a/2/r)^2));
-}    
-}
+        if(fabs(y_axis_val) <= 0.2){
+            if (fabs(x_axis_val)<=0.5){
+                vel.left_front_vel = 1500;
+                vel.right_front_vel = 1500;
+                vel.left_middle_vel = 1500;
+                vel.right_middle_vel = 1500;
+                vel.left_back_vel = 1500;
+                vel.right_back_vel = 1500;
+            }
+            else if(x_axis_val<-0.5){
+                float x_axis_val_mod2= x_axis_val*0.4-0.3;
+                vel.left_front_vel = -c2*x_axis_val_mod2+1500;
+                vel.right_front_vel = c2*x_axis_val_mod2+1500;
+                vel.left_middle_vel = -c2*x_axis_val_mod2+1500;
+                vel.right_middle_vel = c2*x_axis_val_mod2+1500;
+                vel.left_back_vel = -c2*x_axis_val_mod2+1500;
+                vel.right_back_vel = c2*x_axis_val_mod2+1500;
+            }
+        }
+        else{
+            vel.left_front_vel = v1 + 1500;
+            vel.right_front_vel = v3 + 1500;
+            vel.left_middle_vel = v2 + 1500;
+            vel.right_middle_vel = v4 + 1500;
+            vel.left_back_vel = v1 + 1500;
+            vel.right_back_vel = v3 + 1500;
+        }   
 
-else if(y_axis_val<=-0.1){
-    if(x_axis_val >= 0){
-    if(x_axis_val>0.8 && (fabs(y_axis_val) <=0.2)){
-    vel.left_front_vel = c2/n + 1500;
-    vel.right_front_vel = -c2/n + 1500;
-    vel.left_middle_vel = c2/n + 1500;
-    vel.right_middle_vel = -c2/n + 1500;
-    vel.left_back_vel = c2/n + 1500;
-    vel.right_back_vel = -c2/n + 1500;
-    }
-    else{
-    vel.left_front_vel = v1/5 + 1300;
-    vel.right_front_vel = v3/5 + 1300;
-    vel.left_middle_vel = v4/5 + 1300;
-    vel.right_middle_vel = v2/5 + 1300;
-    vel.left_back_vel = v1/5 + 1300;
-    vel.right_back_vel = v3/5 + 1300;
-    }
-    // thetaL=asin((a/2/r)/sqrt(dem));
-    // thetaR=asin((a/2/r)/sqrt(1+(a/2/r)^2));    
-    }
-    else if(x_axis_val < 0){
-    if(x_axis_val<-0.8 && (fabs(y_axis_val) <= 0.2)){
-    vel.left_front_vel = -c2/n + 1500;
-    vel.right_front_vel = c2/n + 1500;
-    vel.left_middle_vel = -c2/n + 1500;
-    vel.right_middle_vel = c2/n + 1500;
-    vel.left_back_vel = -c2/n + 1500;
-    vel.right_back_vel = c2/n + 1500;
-    }
-    else{
-    vel.left_front_vel = v3/5 + 1300;
-    vel.right_front_vel = v1/5 + 1300;
-    vel.left_middle_vel = v2/5 + 1300;
-    vel.right_middle_vel = v4/5 + 1300;
-    vel.left_back_vel = v3/5 + 1300;
-    vel.right_back_vel = v1/5 + 1300;
-    }// thetaL=asin((a/2/r)/sqrt(dem));
-    // thetaR=asin((a/2/r)/sqrt(1+(a/2/r)^2));
+    
 }
-}
-
     vel_pub.publish(vel);
 }
 
