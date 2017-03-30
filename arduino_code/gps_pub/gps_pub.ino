a34db3698d4b5310b26fa8fd68fdf67ea02f6615
Kashyapa <vaibhavrokz13@gmail.com>
2017-03-30 18:40:05 +0530

    First commit

--- arduino_code/gps_pub/gps_pub.ino
+++ arduino_code/gps_pub/gps_pub.ino
@@ -3,16 +3,17 @@
 #include<Arduino.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+//Incuding all the necessary headers and messages
+ros::NodeHandle nh; //initiating the node handler
+rover_msgs::GPS values; // creating a GPS message and naming it values
 
-ros::NodeHandle nh;
-rover_msgs::GPS values;
+ros::Publisher GPS ("test1",&values); // initialising the publisher for the GPS
 
-ros::Publisher GPS ("test1",&values);
 
-static const int RXPin = 4, TXPin = 3;
-static const uint32_t GPSBaud = 4800;
+static const int RXPin = 4, TXPin = 3; // initiating the static constants for pins of arduino
+static const uint32_t GPSBaud = 4800;  // initiating the baud rate of the gps communication link
 
-TinyGPSPlus gps;
+TinyGPSPlus gps; // creating a tinygps object
 
 
 SoftwareSerial ss(RXPin, TXPin);
@@ -20,27 +21,31 @@ SoftwareSerial ss(RXPin, TXPin);
 
 void setup()
 {
+  // initialising the communication with the arduino at 115200 baud rate
   Serial.begin(115200);
   ss.begin(GPSBaud);
 
 
   nh.initNode();
+  // advertising the nodehandler to the GPS
   nh.advertise(GPS);
-  
+
 }
 
 void loop()
 {
-  
+
+  // Getting the latitude and longitude of the position of the rover
   while (ss.available() > 0)
     if (gps.encode(ss.read()))
-     {
+    {
       values.inst_lat= gps.location.lat();
       values.inst_lng = gps.location.lng();
-     }
-
-     GPS.publish(&values);
-     nh.spinOnce();
+    }
+//publishing the values to the message
+  GPS.publish(&values);
+  nh.spinOnce();
 }
+
 
 
