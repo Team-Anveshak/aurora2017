2ee072c1846e103e589ab1b68d234dd5f85c1f84
aniketmore234 <aniket.more305@gmail.com>
2017-05-16 15:21:09 +0530

    adding Router orientation code

--- src/sensors/src/gpsRouter.cpp
+++ src/sensors/src/gpsRouter.cpp
@@ -3,43 +3,42 @@
 #include <rover_msgs/RouterAngle.h>
 #include <cstdlib>
 #include <cmath>
+#include <time.h>
 
 #define PI 3.14159
 
-double lat_initR = 12.99178849*PI/180,logg_initR = 80.2310192*PI/180, dist_init;
-double lat_initB = 12.99978849*PI/180,logg_initB = 80.2910192*PI/180;
-double lat,logg;
-double dist,dist_diff;
-double theta,phi; //phi to be hard coded (initial angle)
+time_t start, end;
 
+double lat_initR = 12.99178849*PI/180,logg_initR = 80.2310192*PI/180; //intial coordinates of router i.e. base station
+double lat,logg; //gps coordinates of rover time dependent
+double phi,theta; //initial angle wrt geographic north
+ 
 void gpsCallback(const sensor_msgs::NavSatFix::ConstPtr& msg){
 	lat = (msg->latitude)*PI/180;
 	logg = (msg->longitude)*PI/180;
-	dist = (sqrt((lat_initR-lat)*(lat_initR-lat)*100 + (logg_initR-logg)*(logg_initR-logg)*100))/10.0;
-	dist_diff = (sqrt((lat_initB-lat)*(lat_initB-lat)*100 + (logg_initB-logg)*(logg_initB-logg)*100))/10.0;
-	theta =	acos((dist*dist + dist_init*dist_init - dist_diff*dist_diff)/(2*dist*dist_init));
+	theta = atan2((sin(logg_initR - logg))*(cos(lat_initR)),(cos(lat))*(sin(lat_initR))-(sin(lat))*(cos(lat_initR)*(cos(logg_initR-logg))));
+	if(theta<0) theta = theta + 2*PI;
 }
 
 int main(int argc,char **argv){
 	ros::init(argc,argv,"gps");
 	ros::NodeHandle n;
 
-	ros::Subscriber gps_sub = n.subscribe("/phone1/android/fix",50,gpsCallback);
-	ros::Publisher angle_pub = n.advertise<rover_msgs::RouterAngle>("/rover1/angle",50);
+	//ros::Subscriber gps_router = n.subscribe("/phone1/android/fix",50,gpsCallback);
+	ros::Publisher angle_pub = n.advertise<rover_msgs::RouterAngle>("/rover1/routerAngle",50);
 	ros::Rate loop_rate(5);	
-	dist_init=sqrt((lat_initB-lat_initR)*(lat_initB-lat_initR)*100 + (logg_initB-logg_initR)*(logg_initB-logg_initR)*100)/10.0;
 
 	while(ros::ok()){
 		ros::spinOnce();
 		rover_msgs::RouterAngle Angle;
-		if(theta>phi){
-			Angle.angle = theta-phi*180/PI;
-		}
-		else{
-			Angle.angle = theta-phi*180/PI;
-		}
-		//ROS_INFO("%lf",theta-phi);
-	
+		Angle.angle = (theta*180/PI-phi)/2;
+		ROS_INFO("%f\n",Angle.angle);
+		time(&start);
+		time(&end);
+		while((difftime(end,start) < 10))
+		{
+       	time(&end);
+        }
 		angle_pub.publish(Angle);
 		loop_rate.sleep();		
 	}
